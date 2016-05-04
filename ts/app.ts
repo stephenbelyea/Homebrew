b80f91dc6356ddc3ff9b874ec20cb1159328ddb2
Stephen Belyea <belyea.stephen@gmail.com>
2016-05-03 21:11:15 -0400

    Update to typings.

--- ts/app.ts
+++ ts/app.ts
@@ -1,5 +1,11 @@
-/// <reference path="../typings/tsd.d.ts"/>
+/// <reference path="../typings/main.d.ts"/>
+
+class Test {
+  constructor(){
+    console.log("hello");
+  };
+}
 
 jQuery(document).ready(function($){
-
+  let test = new Test();
 });
